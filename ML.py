85c1bcd8894dd11eb2b9b3828b930d37140be4bf
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-04-08 10:32:28 +0300

    add code

--- ML.py
+++ ML.py
@@ -4,8 +4,6 @@ test = pd.read_csv("test.csv")
 train = pd.read_csv("train.csv")
 test.head(4)
 train.head(4)
-#смотрю первые четыре строки
-#c = f.readlines()
-#print(c[:5])
-#смотрю количество записей
-#print(len(c))
+#если pandas, то
+test.describe()
+train.describe()
