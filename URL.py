b827c12469d7988663f17dca805f3c3c1515a2af
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-04-22 12:38:14 +0300

    Update URL.py

--- URL.py
+++ URL.py
@@ -5,12 +5,12 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.snowball import SnowballStemmer
+
 st.title('Фейковые ссылки')
-title = st.text_input('Введите ссылку', value='')
+title = st.text_input('Введите ссылку', value='https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html', key='input_url')
 
 #Загрузка модели
 @st.cache(allow_output_mutation=True)
-
 def load_model():
     movies = pickle.load(open('myfile.pkl','rb'))
     model = pickle.load(movies)
@@ -28,15 +28,19 @@ stemmer = SnowballStemmer("english")
 cv = CountVectorizer()
 
 #Функция для определения СПАМа
-def prepare_data(X) :
+def prepare_data(title, non_optional_func=None):
+    if not title:
+        return None, None
+    X = pd.DataFrame({'url': [title]})
     X['text_tokenized'] = X.url.map(lambda t: tokenizer.tokenize(t)) #Разделение на токены
     X['text_stemmed'] = X.text_tokenized.map(lambda t: [stemmer.stem(word) for word in t])#stemmer приводит слова с одним корнем к одному слову
     X['text_sent'] = X.text_stemmed.map(lambda t: ' '.join(t)) #Объеденяем список в предложение
     features = cv.fit_transform(X.text_sent)
     return X, features
-X, features = prepare_data(X)
 
-if result:
+X, features = prepare_data(title)
+
+if result and features is not None:
     model = load_model()
     y_pred = model.predict(features)
     if y_pred[0] == 0:
