f27a563a108de84de306f2cfb905b7a317295d21
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-06-13 23:25:30 +0300

    Update URL.py

--- URL.py
+++ URL.py
@@ -30,7 +30,7 @@ cv = CountVectorizer()
 
 #Функция для определения СПАМа
 
-def prepare_data(title):
+def prepare_data(title, non_optional_func=None):
     if not title:
         return None, None
     X = pd.DataFrame({'url': [title]})
