7f7e8cdf722228bba694e20e5f5c483f8d533eb2
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-06-17 22:50:42 +0300

    changes

--- ML.py
+++
@@ -1,9 +0,0 @@
-#читаю файл
-import pandas as pd
-test = pd.read_csv("test.csv")
-train = pd.read_csv("train.csv")
-print(test.head(4))
-print(train.head(4))
-#если pandas, то
-print (test.describe())
-print (train.describe())
