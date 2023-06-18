d21eb24e9732e4b1b42c16c3bd537330488f644f
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-06-18 15:05:36 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pickle
 import numpy as np
-from PIL import  Image
+from PIL import Image
 
-st.header(" Добро пожаловать! 👋")
+st.header("Добро пожаловать! 👋")
 st.info("Группа 9: Смирнова А., Кожедуб Н., Багаудинов Э., Петраков В.")
 
 display = Image.open('images/display.jpg')
@@ -14,19 +14,21 @@ st.title('Обнаружение фишинговых URL-адресов')
 title = st.text_input('Введите ссылку', 'вот тут')
 title = [title]
 
+
 # Загрузка модели
-@st.cache_data
+@st.cache
 def load_model():
     model = pickle.load(open('phish.pkl', 'rb'))
     return model
 
-result = st.button('🤗Распознать')
+
+result = st.button('🤗 Распознать')
 if result:
     model = load_model()
     y_pred = model.predict(title)
     if y_pred[0] == 0:
-        st.success("Этот URL является безопасным",icon="✅")
+        st.success("Этот URL является безопасным", icon="✅")
         st.image('images/class.jpg')
     else:
-         st.warning("Этот URL является вредоносным", icon="⚠️")
-         st.image('images/warnings.png')
+        st.warning("Этот URL является вредоносным", icon="⚠️")
+        st.image('images/warnings.png')
