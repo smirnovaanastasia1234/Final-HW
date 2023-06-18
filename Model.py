dd7613a7f4ccbd984b8a607e654a8b71334e6737
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-06-18 14:49:20 +0300

    changes streamlit

--- Model.py
+++ Model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.snowball import SnowballStemmer
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,19 @@
 import streamlit as st
 import pickle
 import numpy as np
+from PIL import  Image
 
-st.title('Фейковые ссылки')
+st.header(" Добро пожаловать! 👋")
+st.info("Группа 9: Смирнова А., Кожедуб Н., Багаудинов Э., Петраков В.")
+
+display = Image.open('images/display.jpg')
+display = np.array(display)
+st.image(display)
+
+st.title('Обнаружение фишинговых URL-адресов')
 title = st.text_input('Введите ссылку', 'вот тут')
 title = [title]
+
 # Загрузка модели
 @st.cache_data
 def load_model():
@@ -16,6 +25,8 @@ if result:
     model = load_model()
     y_pred = model.predict(title)
     if y_pred[0] == 0:
-        st.write("Этот URL является безопасным")
+        st.success("Этот URL является безопасным",icon="✅")
+        st.image('images/class.jpg')
     else:
-         st.write("Этот URL является вредоносным")
+         st.warning("Этот URL является вредоносным", icon="⚠️")
+         st.image('images/warnings.png')
